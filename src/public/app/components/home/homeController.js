6c9c3a3f53a7147df4970fbff9814672315ca03a
Michael Rodrigues <michaeldf.ti@gmail.com>
2017-03-23 22:22:07 -0300

    - Base Project - Structured project - HTML and SASS ready.

---
+++ src/public/app/components/home/homeController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by michaeldfti on 21/02/17.
+ */
+
+angular
+    .module('TicTacToe')
+    .controller('homeController', homeController);
+
+homeController.$inject = ['$scope'];
+
+function homeController($scope) {
+
+
+
+}
